a8bddbeee9aa40756df6da41defe4d03f8ba8c99
Gijs van Steenpaal <gijs.van.steenpaal@gmail.com>
2020-06-16 17:02:32 +0200

    Make hello cuda example clearer

--- kernels/hello-world.cu
+++ kernels/hello-world.cu
@@ -2,5 +2,5 @@
 #include<stdlib.h>
 
 extern "C" __global__ void hello_world(void) {
-    printf("Hello World! from thread [%d,%d] From device\n", threadIdx.x,blockIdx.x);
+    printf("Hello World! from thread [%d,%d] on GPGPU\n", threadIdx.x, blockIdx.x);
 }
