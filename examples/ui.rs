56f1a75316f2ceb49a3b442ceac8858bd79dda2d
Gijs van Steenpaal <gijs.van.steenpaal@gmail.com>
2020-08-31 15:16:32 +0200

    Add ui example for testing

--- examples/ui.rs
+++ examples/ui.rs
@@ -1,44 +1,63 @@
 extern crate kiss3d;
-extern crate nalgebra as na;
 
 // Third party
-use kiss3d::light::Light;
-use kiss3d::window::{State, Window};
+use kiss3d::window::Window;
 
 // Conrod
 use kiss3d::conrod::{widget, widget_ids, Color, Colorable, Positionable, Sizeable, Widget};
-use na::{Point2, Point3};
 
 // Generate a unique `WidgetId` for each widget.
 widget_ids! {
     pub struct Ids {
-        text,
-        canvas
+        canvas,
+        text_left,
+        text_right,
+        pane_left,
+        pane_right
     }
 }
+
+const PANE_WIDTH: kiss3d::conrod::Scalar = 150.0;
+
 
 // Main display function
 pub fn main() {
     const WINDOW_WIDTH: u32 = 1024;
     const WINDOW_HEIGHT: u32 = 768;
-    let mut window = Window::new_with_size("Pointctl visualizer", WINDOW_WIDTH, WINDOW_HEIGHT);
+    let mut window =
+        Window::new_with_size("Pointctl UI testing program", WINDOW_WIDTH, WINDOW_HEIGHT);
     window.set_background_color(1.0, 1.0, 1.0);
-    window.set_light(Light::StickToCamera);
-    window.set_point_size(4.);
 
     let ids = Ids::new(window.conrod_ui_mut().widget_id_generator());
 
-    // Start the render loop, this will _not_ work with 2D scenes yet.
     while window.render() {
         let mut ui = window.conrod_ui_mut().set_widgets();
-        // widget::Canvas::new()
-        //     // .align_bottom()
-        //     .h(100.0)
-        //     .set(ids.canvas, &mut ui);
 
-        widget::Text::new("test")
-            // .mid_top_of(ids.canvas)
+        // Create the complete canvas
+        widget::Canvas::new()
+            .color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+            .set(ids.canvas, &mut ui);
+
+        // Create left pane on the main canvas
+        widget::Canvas::new()
+            .align_left_of(ids.canvas)
+            .w(PANE_WIDTH)
+            .set(ids.pane_left, &mut ui);
+
+        // Create right pane on the main canvas
+        widget::Canvas::new()
+            .align_right_of(ids.canvas)
+            .w_of(ids.pane_left)
+            .set(ids.pane_right, &mut ui);
+
+        widget::Text::new("Left test")
+            .mid_top_of(ids.pane_left)
             .color(Color::Rgba(1.0, 0.0, 0.0, 1.0))
-            .set(ids.text, &mut ui);
+            .set(ids.text_left, &mut ui);
+
+        widget::Text::new("Right test")
+            .mid_top_of(ids.pane_right)
+            .color(Color::Rgba(1.0, 0.0, 0.0, 1.0))
+            .set(ids.text_right, &mut ui);
     }
 }
---
+++ notes/TODO.md
@@ -0,0 +1,10 @@
+# What to do!
+
+ - [ ] Text overlay using conrod
+    - [ ] Basic text
+    - [ ] Color mapping display
+    - [ ] Buttons
+    - [ ] Gamma slider
+ - [ ] Van Driel explanation
+ - [ ] 3D continous with billboarding in vertex shader
+ - [ ] Point selection
