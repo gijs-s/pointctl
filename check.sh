13f05ac67de885ad7082c76747541f887a67a806
Gijs van Steenpaal <gijs.van.steenpaal@gmail.com>
2020-10-07 16:36:53 +0200

    Update the cargo check script

--- check.sh
+++ check.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-# check if code is even compilable
+echo "\033[1;36mRunning Cargo check\033[0m"
+echo ""
 cargo check --all
-# Run formater
-cargo fmt --all -- --write-mode=diff
+
+# Run formater check
+echo ""
+echo "\033[1;36mRunning Cargo format\033[0m"
+echo ""
+cargo fmt --all -- --check
+
 # run common mistake checker
+echo ""
+echo "\033[1;36mRunning Cargo clippy\033[0m"
+echo ""
 cargo clippy --all --verbose -- -D warnings
+
 # Run tests
+echo ""
+echo "\033[1;36mRunning the tests\033[0m"
+echo ""
 cargo test --all
