5ad4ae93d3dcd60752bc374d873898a37520196b
Gijs van Steenpaal <gijs.van.steenpaal@gmail.com>
2020-10-10 13:26:11 +0200

    Add stub module for search structures
    This module will contain the data structure used to store all the points

---
+++ src/search/mod.rs
@@ -0,0 +1,4 @@
+/// This module contains the data structure that will be used to store
+/// the nd/low-d points and all its annotations. The annotations will
+/// contain index, explanations and normals. The search structure will
+/// use rtree's to quickly find nd or low-d neighbors and all its data
