58fa1ed27c41e86624440e57685b40c70ae0dc1e
Gijs van Steenpaal <gijs.van.steenpaal@gmail.com>
2020-10-24 14:18:53 +0200

    Add progress bar example

---
+++ examples/progress-bar.rs
@@ -0,0 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
+use indicatif::{ProgressBar, ProgressStyle, ProgressIterator};
+
+fn main() {
+    // let mut searched_neighborhoods = 0;
+    let total_size = 10_015;
+
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta} left at {per_sec})")
+        .progress_chars("#>-"));
+
+    let _res: Vec<u64> = (0..total_size).progress_with(pb).map(|v| {thread::sleep(Duration::from_millis(12)); v}).collect();
+}
